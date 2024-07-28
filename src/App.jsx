99b0198d08f29f6747b5ef1523b0825028c83a4d
ishitagit2003 <ishitagupta2003@gmail.com>
2024-07-28 14:11:47 +0530

    about page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import styles from './App.module.css';
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./components/Home/Home";
+import { About } from "./components/About/About";
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <div className={styles.App}>
       <Navbar/>
       <Home/>
+      <About/>
       </div>
      
     </>
