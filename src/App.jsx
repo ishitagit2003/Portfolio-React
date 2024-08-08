9e20008fdaff7a4aa35b1f287295f6f2937a8d65
ishitagit2003 <ishitagupta2003@gmail.com>
2024-08-09 03:51:37 +0530

    project page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./components/Home/Home";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <Home/>
       <About/>
       <Experience/>
+      <Projects />
+      <Contact />
       </div>
     </>
   )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,12 +9,17 @@ export const Home= () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Ishita</h1>
         <p className={styles.description}>
-          I'm a MERN developer based in Ghaziabad,Uttar Pradesh,India.
+          I'm a MERN developer based in Ghaziabad, Uttar Pradesh, India.<br></br>
           I’ve built numerous backend servers and frontend applications. I’m passionate about creating new, user-friendly apps with attractive designs. 
         </p>
+        <div className={styles.btns}>
+        <button className={styles.cvBtn} onClick={() => window.open('./assets/Resume Ishita.pdf')}>
+            Download CV
+          </button>
         <a href="mailto:ishitagupta2003@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
+        </div>
       </div>
       <img
         src={getImageUrl("home/homeImage.png")}
